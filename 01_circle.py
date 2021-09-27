458b12ce4b4681c41d1ab23e7edc66a8fec4f318
Nikolay Silin <silinnikolay@yandex.ru>
2021-09-27 23:57:05 +0500

    completed the second exercise

--- 01_circle.py
+++ 01_circle.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Есть значение радиуса круга
@@ -9,7 +8,11 @@ radius = 42
 #       формулу можно подсмотреть в интернете,
 #       пи возьмите равным 3.1415926
 #       точность указывается в функции round()
-# TODO здесь ваш код
+
+square = 3.1415926 * radius ** 2
+square_4 = (round(square, 4))
+square_4 = str(square_4)
+print('Square of a circle = ' + square_4)
 
 
 # Далее, пусть есть координаты точки
@@ -23,14 +26,27 @@ point = (23, 34)
 #       формула так же есть в интернете
 #       квадратный корень - это возведение в степень 0.5
 #       операции сравнения дают булевы константы True и False
-# TODO здесь ваш код
+
+center = (0, 0)
+
+center_point_distance = ((point[0] - center[0]) ** 2 + (point[1] - center[1]) ** 2) ** 0.5
+center_point_distance = str(center_point_distance)
+point_in_a_circle = ((point[0] - center[0]) ** 2 + (point[1] - center[1]) ** 2) < radius ** 2
+point_in_a_circle = str(point_in_a_circle)
+print('Distance from point to center = ' + center_point_distance)
+print("Point in a circle = " + point_in_a_circle)
 
 # Аналогично для другой точки
 point_2 = (30, 30)
 # Если точка point_2 лежит внутри круга (radius = 42), то выведите на консоль True,
 # Или False, если точка лежит вовне круга.
-# TODO здесь ваш код
 
+center_point_2_distance = ((point_2[0] - center[0]) ** 2 + (point_2[1] - center[1]) ** 2) ** 0.5
+center_point_2_distance = str(center_point_2_distance)
+point_2_in_a_circle = ((point_2[0] - center[0]) ** 2 + (point_2[1] - center[1]) ** 2) < radius ** 2
+point_2_in_a_circle = str(point_2_in_a_circle)
+print('\nDistance from point_2 to center = ' + center_point_2_distance)
+print("Point_2 in a circle = " + point_2_in_a_circle)
 # Пример вывода на консоль:
 #
 # 77777.7777
